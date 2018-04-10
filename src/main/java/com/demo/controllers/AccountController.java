c8692413878571beb167a57bf43655b42440f2f0
glenndennismorales <wletmp5@yahoo.com>
2018-04-09 22:49:14 -0400

    add mq

--- src/main/java/com/demo/controllers/AccountController.java
+++ src/main/java/com/demo/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.demo.controllers;
 import com.demo.coreapi.LockAccountCommand;
 import com.demo.coreapi.StartCardControlCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,10 @@ public class AccountController {
     @PostMapping("/accounts/{accountId}/lock")
     public Future<String> lockAccount(@PathVariable String accountId){
         return commandGateway.send(new LockAccountCommand(accountId));
+    }
+
+    @GetMapping("/hello")
+    public String test(){
+        return "Test";
     }
 }
